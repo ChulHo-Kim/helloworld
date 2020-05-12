5b8f00e1d7b1eb268c98f9c7d203affa155461a9
blackelder7 <blackelder7@gmail.com>
2020-05-12 15:54:38 +0900

    text modify

--- src/main/java/tangunsoft/helloworld/SampleController.java
+++ src/main/java/tangunsoft/helloworld/SampleController.java
@@ -13,7 +13,7 @@ public class SampleController {
     @ResponseBody
     public String sampleHome() {
 
-        return "Hello Tangunsoft.";
+        return "Hello Tangunsoft. Hello ETRI.";
 
     }
 }
